d3632f91d237753ca482da1fad913e79a3d87eeb
root <madrid@D25pc2>
2017-11-10 15:16:23 +0100

    Comentarios y envío a EducaMadrid

--- bme280_OLED_simple/bme280_OLED_simple.ino
+++ bme280_OLED_simple/bme280_OLED_simple.ino
@@ -1,15 +1,15 @@
-#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
-#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
-#include <Wire.h>  // Include Wire if you're using I2C
-#include <SFE_MicroOLED.h>  // Include the SFE_MicroOLED library
+#include <ESP8266WiFi.h>  
+#include <DNSServer.h>
+#include <Wire.h>  	  
+#include <SFE_MicroOLED.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #define PIN_RESET 255  //
 #define DC_JUMPER 0  // I2C Addres: 0 - 0x3C, 1 - 0x3D
 #define SEALEVELPRESSURE_HPA (1013.25)
-/////////////////////////////////
-// DATOS DEL CENTRO EDUCATIVO  //
-/////////////////////////////////
+//////////////////////////////////////////////
+// DATOS DE CONEXIÓN WIFI Y A LA PLATAFORMA //
+//////////////////////////////////////////////
 char ssid[32] = "SSID DE LA WIFI";
 char password[32] = "CONTRASEÑA DE LA WIFI";
 String proveedor = "IDENTIFICADOR DEL PROVEEDOR";
@@ -32,7 +32,7 @@ Adafruit_BME280 bme; // I2C
 void setup() {
   Serial.begin(9600);
   bme.begin();
-  escribirIni();  //Escribe la pantalla de bienvenida en la OLED
+  escribirIni();  //Escribe la pantalla de bienvenida en la OLED, se debe cambiar el centro educativo en la función escribirIni()
   delay(1000);
 }
 
@@ -43,9 +43,9 @@ void loop()
   float p = bme.readPressure() / 100.0F;// Presión
   float a = bme.readAltitude(SEALEVELPRESSURE_HPA);// Altura
   float h = bme.readHumidity();// Humedad
-  //ESCRIBIMOS EN LA PANTALLA LED
-  oled.setCursor(0, 0);  // Set the text cursor to the upper-left of the screen.
-  oled.print("T: "); // Print a const string
+  //ESCRIBIMOS EN LA PANTALLA LED LOS DATOS MEDIDOS EN CADA LOOP
+  oled.setCursor(0, 0);
+  oled.print("T: ");
   oled.print(t, 1);
   oled.print(" *C");
   oled.setCursor(0, 10);
@@ -60,20 +60,20 @@ void loop()
   oled.print("H: ");
   oled.print(h, 1);
   oled.println(" %");
-  oled.display(); // Draw to the screen
-  //CADA 30 SEGUNDOS CONEXIÓN A WIFI, CONEXIÓN AL SERVIDOR, ENVÍO Y DESCONEXIÓN
-  if (millis() > ultimo_envio + 30000) {//Sólo envío datos a thingspeak cada 30 segundos
+  oled.display();
+
+  // CADA 10 MINUTOS SE REALIZA LA CONEXIÓN A LA WIFI, CONEXIÓN AL SERVIDOR, ENVÍO Y DESCONEXIÓN DEL SERVIDOR Y DESCONEXIÓN DE LA WIFI.
+  if (millis() > ultimo_envio + 600000) {
     Serial.println("CONEXIÓN A LA WIFI");
     while (WiFi.status() != WL_CONNECTED) {
       texto = "CONEC W";
       escribir(texto);
-      //CONEXIÓN A LA RED WIFI
-      WiFi.begin(ssid, password);//Nos conectamos a la red wifi definida.
-      Serial.println("CONECTADO ....");
-      texto = "CONEC S";
+      // CONEXIÓN A LA RED WIFI
+      WiFi.begin(ssid, password);// Nos conectamos a la red wifi definida.
+      texto = "CONECTADO A LA WIFI";
       escribir(texto);
       delay(5000);
-      Serial.println(WiFi.localIP());
+      Serial.println(WiFi.localIP());  // Muestra por el monitor serie del IDE de arduino la IP de Wemos para comprobar si todo ha ido bien.
     }
       String TemValor = String(t);
       parametro = "temperatura";
@@ -86,7 +86,6 @@ void loop()
       enviar(HumValor, parametro);
       ultimo_envio = millis();
       WiFi.disconnect();
-      Serial.println("desconectado de la wifi");
   }
 }
 //////////////////////////////////////////////////////////////
@@ -103,7 +102,7 @@ void enviar(String valor, String parametro){
     client.println("Host: iot.educa.madrid.org:8081");
     client.println("Connection: Keep-Alive");
     client.println("User-Agent: Apache-HttpClient/UNAVAILABLE (java 1.4)");
-    client.println(); //este espacio es imprescindible
+    client.println();
   }
   client.stop();
   delay(2000);
@@ -114,15 +113,15 @@ void enviar(String valor, String parametro){
 void escribir(String valor){
       oled.setCursor(0, 40);
       oled.print(valor);
-      oled.display();   // Display what's in the buffer (splashscreen)
+      oled.display();
       oled.setCursor(0, 40);
       oled.print("       ");
-      oled.display();   // Display what's in the buffer (splashscreen)
+      oled.display();
       delay(2000);
 }
-/////////////////////////////////////
+//////////////////////////////////////////////////
 // ESCRIBIMOS INICIALMENTE EN LA PANTALLAS OLED //
-/////////////////////////////////////
+//////////////////////////////////////////////////
 void escribirIni(){
   oled.begin();     // Initialize the OLED
   oled.clear(PAGE); // Clear the display's internal memory
@@ -132,7 +131,7 @@ void escribirIni(){
   oled.setCursor(0, 0);  // Set the text cursor to the upper-left of the screen.
   oled.print("Configure"); // Print a const string
   oled.setCursor(0, 10);  // Set the text cursor to the upper-left of the screen.
-  oled.print("CRIF LAS ACACIAS"); // Print a const string
+  oled.print("NOMBRE DEL CENTRO EDUCATIVO"); // Print a const string
   oled.setCursor(0, 20);  // Set the text cursor to the upper-left of the screen.
   oled.print("BIENVENIDO"); // Print a const string
   oled.display();   // Display what's in the buffer (splashscreen)
